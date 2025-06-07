ac95403f9257320446ed54ec92783121e98ae236
FrancescoSanti96 <francescosanti123@gmail.com>
2025-06-07 19:17:40 +0200

    feat: impelmetnation of home bollettini agricoltori example

---
+++ src/Template.Web/Areas/Agricoltore/Models/BollettinoCardViewModel.cs
@@ -0,0 +1,12 @@
+namespace Template.Web.Areas.Agricoltore.Models
+{
+    public class BollettinoCardViewModel
+    {
+        public string Title { get; set; }
+        public string Tag1 { get; set; }
+        public string Tag2 { get; set; }
+        public string Description { get; set; }
+        public string ViewLink { get; set; }
+        public string DownloadLink { get; set; }
+    }
+} 
