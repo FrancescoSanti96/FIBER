7c1cbca685c76b1f0db337c909bc687a688fe62a
FrancescoSanti96 <francescosanti123@gmail.com>
2025-06-07 18:47:07 +0200

    feat: first agricoltor view fe

---
+++ src/Template.Web/Areas/Agricoltore/Controllers/AgricoltoreController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Web.Areas.Agricoltore.Controllers
+{
+    [Area("Agricoltore")]
+    public partial class AgricoltoreController : Controller
+    {
+        public virtual IActionResult BollettiniAgricoltore()
+        {
+            return View();
+        }
+
+        public virtual IActionResult ImpostazioniAgricoltore()
+        {
+            return View();
+        }
+
+        public virtual IActionResult Bollettino()
+        {
+            return View();
+        }
+    }
+} 
--- src/Template.Web/Startup.cs
+++ src/Template.Web/Startup.cs
@@ -113,6 +113,7 @@ namespace Template.Web
                 endpoints.MapHub<TemplateHub>("/templateHub");
 
                 endpoints.MapAreaControllerRoute("Example", "Example", "Example/{controller=Users}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute("Agricoltore", "Agricoltore", "Agricoltore/{controller=Agricoltore}/{action=BollettiniAgricoltore}/{id?}");
                 endpoints.MapControllerRoute("default", "{controller=Login}/{action=Login}");
             });
         }
