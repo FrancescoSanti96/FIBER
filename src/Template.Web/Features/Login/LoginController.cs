3bf7bd152fc1761e6d81baf31419a835039d6ffa
FrancescoSanti96 <francescosanti123@gmail.com>
2025-06-22 19:11:45 +0200

    feat: some ui best pratices for the login feedback and some fixes

--- src/Template.Web/Features/Login/LoginController.cs
+++ src/Template.Web/Features/Login/LoginController.cs
@@ -130,7 +130,8 @@ namespace Template.Web.Features.Login
                 }
             }
 
-            return RedirectToAction(nameof(Login));
+            // Torna alla view con gli errori invece di fare redirect
+            return View(model);
         }
 
         [HttpPost]
