7d2d8f164d86323e7a72bf8a330c87f468f6ff64
FrancescoSanti96 <francescosanti123@gmail.com>
2025-06-29 12:14:11 +0200

    remove tecnico impostazioni

--- src/Template.Web/Areas/Tecnico/TecnicoController.cs
+++ src/Template.Web/Areas/Tecnico/TecnicoController.cs
@@ -41,12 +41,6 @@ namespace Template.Web.Areas.Tecnico
             return View(model);
         }
 
-        // GET: Tecnico/Tecnico/ImpostazioniTecnico
-        public virtual IActionResult ImpostazioniTecnico()
-        {
-            return View();
-        }
-
         // GET: Tecnico/Tecnico/NuovoBollettino
         public virtual IActionResult NuovoBollettino()
         {
